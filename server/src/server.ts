0fcaa916d9db29a4edab4a0d7d27005d379912c5
Danijel Garic <danijel.garic@arvato-scs.com>
2024-11-03 11:04:49 +0100

    refactor

--- server/src/server.ts
+++ server/src/server.ts
@@ -43,9 +43,6 @@ export class Server {
         this.server.use('/api/images', express.static(path.join(__dirname, 'images')));
 
         this.server.get('/*', (req: Request, res: Response) => {
-            console.log(this.distFolder, ' <------ distFolder ------ ');
-            console.log(this.publicFolder, ' <------ publicFolder ------ ');
-
             res.sendFile(this.distFolder + '/index.html');
         });
 
