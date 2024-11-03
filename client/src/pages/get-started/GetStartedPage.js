78f0666d48e0a61747641c357f1a906a2753f558
Danijel Garic <danijel.garic@arvato-scs.com>
2024-11-03 14:39:40 +0100

    fix project delete button

--- client/src/pages/get-started/GetStartedPage.js
+++ client/src/pages/get-started/GetStartedPage.js
@@ -38,6 +38,10 @@ Desktop:~/projects/quickt$ docker-compose up
 
             <DwHeader navigation={getMainNavigation()}>
                 <div id="logo"><img src="/logo.png" alt="quickT"/>quickT</div>
+                <div id="extras" className="dw-settings d-flex"><a target="_blank"
+                                                                   href="https://www.paypal.com/donate/?hosted_button_id=CTVTA7GG8AVQU"><img
+                    height="40" src="/coffee.png"
+                    alt="coffee"/></a></div>
             </DwHeader>
 
             <div className={'container'}>
--- client/src/pages/landingpage/LandingPage.js
+++ client/src/pages/landingpage/LandingPage.js
@@ -18,10 +18,12 @@ export const LandingPage = () => {
 
             <DwHeader navigation={getMainNavigation()}>
                 <div id="logo"><img src="/logo.png" alt="quickT"/>quickT</div>
+                <div id="extras" className="dw-settings d-flex"><a target="_blank" href="https://www.paypal.com/donate/?hosted_button_id=CTVTA7GG8AVQU"><img height="40" src="/coffee.png"
+                                                                                alt="coffee"/></a></div>
             </DwHeader>
 
             <section className="main-box">
-                <div className="row">
+            <div className="row">
                     <div className="box">
                         <span className="text">
                         <h1>
