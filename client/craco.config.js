d434747cfbcc48e731e3ad16f1b782109b28ad0c
Danijel Garic <danijel.garic@arvato-scs.com>
2024-11-14 08:44:12 +0100

    fix

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,23 +1,15 @@
 const path = require('path');
 
-// React Refresh Plugin für HMR in der Entwicklung
-const CracoReactRefreshPlugin = require('craco-plugin-react-refresh');
-
 module.exports = {
   webpack: {
     output: {
       path: path.resolve(__dirname, 'dist'), // Pfad besser mit `path.resolve`
       filename: 'bundle.js',
-      publicPath: '/',
+      publicPath: '/'
     },
     alias: {
       '@components': path.resolve(process.cwd(), './src/components'),
-      '@services': path.resolve(process.cwd(), './src/services'),
+      '@services': path.resolve(process.cwd(), './src/services')
     },
-  },
-  plugins: [
-    ...(process.env.NODE_ENV === 'development'
-        ? [{ plugin: CracoReactRefreshPlugin }]
-        : []),
-  ],
+  }
 };
